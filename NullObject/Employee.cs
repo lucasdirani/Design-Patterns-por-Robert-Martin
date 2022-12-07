751bee8a4ecbd21f60ded549b89bda33dfb07a7f
Lucas Dirani <lruizd@protonmail.com>
2022-12-07 10:12:36 -0300

    Exemplo de código para o padrão Null Object.

---
+++ NullObject/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PatternsUncleBob.NullObject
+{
+    public abstract class Employee
+    {
+        public abstract bool IsTimeToPay(DateTime time);
+        public abstract void Pay();
+
+        public static readonly Employee NULL = new NullEmployee();
+
+        private class NullEmployee : Employee
+        {
+            public override bool IsTimeToPay(DateTime time)
+            {
+                return false;
+            }
+
+            public override void Pay()
+            {
+            }
+        }
+    }
+}
